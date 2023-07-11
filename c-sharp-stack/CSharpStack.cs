19bf924eb0a1cc3a055c84c9a1661a92dfda7650
omranali2111 <aljabri1234@hotmail.com>
2023-07-11 11:06:57 +0400

    stack Implementation

---
+++ c-sharp-stack/CSharpStack.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_stack
+{
+    internal class CSharpStack
+    {
+        int top;
+        int[] stack = new int[10];
+        bool IsEmpty()
+        {
+            return (top < 0);
+        }
+        public CSharpStack()
+        {
+            top = -1;
+        }
+        public  bool Push(int data)
+        {
+            if (top >= 10)
+            {
+                Console.WriteLine("Stack is full");
+                return false;
+            }
+            else
+            {
+                top++;
+                stack[top] = data;
+                return true;
+            }
+        }
+       public int Pop()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack is empty");
+                return 0;
+            }
+            else
+            {
+                top--;
+                int value = stack[top];
+                return value;
+            }
+        }
+        public void Peek()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            else
+                Console.WriteLine($"The top element of Stack is :{stack[top]}");
+        }
+        public void PrintStack()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack empty");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Items in the Stack are :");
+                for (int i = top; i >= 0; i--)
+                {
+                    Console.WriteLine(stack[i]);
+                }
+            }
+        }
+    }
+}
---
+++ c-sharp-stack/Program.cs
@@ -0,0 +1,22 @@
+﻿using c_sharp_stack;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CSharpStack stack1 = new CSharpStack();
+
+        stack1.Push(10);
+        stack1.Push(20);
+        stack1.Push(30);
+        stack1.PrintStack();
+        stack1.Peek();
+        stack1.Pop();
+        Console.WriteLine("this is after pop:");
+        stack1.PrintStack();
+
+
+
+
+    }
+}
